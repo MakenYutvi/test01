3484e2671880eb6739e23be51bdca73c68af85ad
MakenYutvi <60502951+MakenYutvi@users.noreply.github.com>
2021-12-21 19:54:10 +0300

    update lootSystem

--- Assets/Scripts/Gameplay/Loot/SpawnLoot.cs
+++ Assets/Scripts/Gameplay/Loot/SpawnLoot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,23 +11,60 @@ namespace Gameplay.Spaceships
     {
         [SerializeField]
         private Spaceship _spaceship;
-        [SerializeField]
-        private GameObject _gameobject;
-        [SerializeField]
-        private List<GameObject> _lootList;
+        //[SerializeField]
+        //private GameObject _gameobject;
+        //[SerializeField]
+        //private List<GameObject> _lootList;
 
-        
+        [SerializeField]
+        private List<LootComponent> _lootComponents;
+
+        private float _totalScore = 0;
+
+
         void Awake()
         {
-            _spaceship.IsDied +=Spawn;
+            _spaceship.IsDied += Spawn;
+            foreach(var x in _lootComponents)
+            {
+                _totalScore += x.dropСhance;
+            }
+         
         }
+
+        //private void SpawnOld(bool IsDead)//not used
+        //{
+        //    int i = UnityEngine.Random.Range(0, _lootList.Count);
+        //    var a = Instantiate(_lootList[i], this.transform.position, this.transform.rotation);
+        //}
 
         private void Spawn(bool IsDead)
         {
-            int i = Random.Range(0, _lootList.Count);       
-            var a = Instantiate(_lootList[i], this.transform.position, this.transform.rotation);
-         }
-    }
+            float currentChance = UnityEngine.Random.Range(0.0f, _totalScore + 1.0f);
+            float currentValue = 0.0f;
+           for(int i = 0; i < _lootComponents.Count; i++)
+            {
+               currentValue += _lootComponents[i].dropСhance;
+               if (currentValue > currentChance)
+                {
+                    var a = Instantiate(_lootComponents[i].gameObject, this.transform.position, this.transform.rotation);
+                    break;
+                }
+            }
+        }
+
+        [Serializable]
+        private struct LootComponent
+        {
+            [SerializeField]
+            public GameObject gameObject;
+            [SerializeField]
+            public float dropСhance;
+
+        }
+
+
+}
 }
 
 
